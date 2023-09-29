ef9e3300aa8d3cf94630ddb9af754b2cd9e18706
Brian Wherry <bwherry@imperosoftware.com>
2023-09-29 10:04:03 +0100

    Add Karafka and Karafka-Web to app and connect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require "karafka/web"
+
 Rails.application.routes.draw do
   resources :things
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  mount Karafka::Web::App, at: '/karafka'
 
   # Defines the root path route ("/")
   # root "articles#index"
